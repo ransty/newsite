7fe63321b6ff04becd0af8faaa7415d17cc1ecea
Keano Porcaro <porkd002@mymail.unisa.edu.au>
2017-09-15 12:13:23 +0930

    Forgot password lol

--- backend/connectivity.php
+++ backend/connectivity.php
@@ -2,7 +2,7 @@
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'practice');
 define('DB_USER','root');
-define('DB_PASSWORD','');
+define('DB_PASSWORD','E8iopqas');
 
 $con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error());
 $db=mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_error());
